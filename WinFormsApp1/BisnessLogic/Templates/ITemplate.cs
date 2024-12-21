561d7ebc258361b415fd56dadc2b6d691791dbec
Lishchinsky Vyacheslave <lishchinsy@gmail.com>
2024-12-21 19:59:41 +0200

    added template implementation

---
+++ WinFormsApp1/BisnessLogic/Templates/ITemplate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectMeneger.BisnessLogic.Templates
+{
+    public interface ITemplate
+    {
+        public string Name { get; }
+
+        public void AddPath(string path);
+
+    }
+}
---
+++ WinFormsApp1/BisnessLogic/Templates/Template.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectMeneger.BisnessLogic.Templates
+{
+    public class Template : ITemplate
+    {
+        public string Name { get; }
+        private IList<string> Paths { get; }
+
+        public Template(string templateName, IList<string> template)
+        {
+            Name = templateName;
+            Paths = new List<string>(template);
+        }
+
+        public void AddPath(string path)
+        {
+            Paths.Add(path);
+        }
+
+
+    }
+}
